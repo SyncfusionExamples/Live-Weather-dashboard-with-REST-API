ccad8cb377f761f402081bfc63c383354ba49145
Sowndharya S <sowndharya.selladurai@syncfusion.com>
2025-08-08 11:03:15 +0530

    Updated

--- WeatherDashBoardDemo/WeatherDashBoardDemo/App.xaml.cs
+++ WeatherDashBoardDemo/WeatherDashBoardDemo/App.xaml.cs
@@ -9,8 +9,6 @@
 
         protected override Window CreateWindow(IActivationState? activationState)
         {
-            Syncfusion.Licensing.SyncfusionLicenseProvider.RegisterLicense("Ngo9BigBOggjHTQxAR8/V1JEaF5cXmRCd0xyWmFZfVtgcV9FaVZURWYuP1ZhSXxWdk1jXH9bc31QQ2FVVUd9XEI=");
-
             return new Window(new WeatherDetails());
         }
     }
